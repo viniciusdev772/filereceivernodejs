fa683edbdd0537caf63ab514219c05dc3d05ab76
viniciusdev772 <viniciusdev37@gmail.com>
2024-02-19 20:29:19 -0300

    Corrigir exportação de função sign no WAController.js

--- controllers/WAController.js
+++ controllers/WAController.js
@@ -1,6 +1,6 @@
 const WALogin = require("../models/walogin");
 
-export async function sign(req, res) {
+async function sign(req, res) {
   const { numero } = req.body;
 
   const login = await WALogin.create({
@@ -10,3 +10,7 @@ export async function sign(req, res) {
   const uid = login.uid;
   return res.json({ link: `https://cdn.viniciusdev.com.br/wabot/link/${uid}` });
 }
+
+module.exports = {
+  sign,
+};
